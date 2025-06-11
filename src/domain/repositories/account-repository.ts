df800c4327810aa11d7636e789cc8886493ef54b
Jhone-cmd <jhone.1941@gmail.com>
2025-06-11 09:58:35 -0300

    chore: updated domain account repository

--- src/domain/repositories/account-repository.ts
+++ src/domain/repositories/account-repository.ts
@@ -2,5 +2,8 @@ import { Account } from '../entities/account'
 
 export abstract class AccountRepository {
   abstract create(account: Account): Promise<void>
+  abstract findById(id: string): Promise<Account | null>
   abstract findByEmail(email: string): Promise<Account | null>
+  abstract save(account: Account): Promise<void>
+  abstract delete(account: Account): Promise<void>
 }
--- test/repositories/in-memory-account-repository.ts
+++ test/repositories/in-memory-account-repository.ts
@@ -7,10 +7,29 @@ export class InMemoryAccountRepository extends AccountRepository {
   async create(account: Account): Promise<void> {
     this.items.push(account)
   }
+
+  async findById(id: string): Promise<Account | null> {
+    const account = this.items.find(item => item.id.toString() === id)
+
+    if (!account) return null
+
+    return account
+  }
+
   async findByEmail(email: string): Promise<Account | null> {
     const account = this.items.find(item => item.email === email)
 
     if (!account) return null
     return account
+  }
+
+  async save(account: Account): Promise<void> {
+    const accountIndex = this.items.findIndex(item => item.id === account.id)
+    this.items[accountIndex] = account
+  }
+
+  async delete(account: Account): Promise<void> {
+    const accountIndex = this.items.findIndex(item => item.id === account.id)
+    this.items.splice(accountIndex, 1)
   }
 }
